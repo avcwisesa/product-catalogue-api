412a31814f176d4d8ef2d269993d8e3683f2dd4c
Avicenna Wisesa <avcwisesa@gmail.com>
2023-12-15 23:33:35 +0800

    feat: add function to insert product to db

---
+++ model.py
@@ -0,0 +1,26 @@
+import psycopg
+from psycopg import sql
+
+class Product():
+    def __init__(self, sku, title, category, kondisi, qty, price):
+        self.sku = sku
+        self.title = title
+        self.category = category
+        self.kondisi = kondisi
+        self.qty = qty
+        self.price = price
+
+    def save(self):
+        insert_query = f"""
+            INSERT INTO product (sku, title, category, kondisi, qty, price)
+            VALUES ('{self.sku}', '{self.title}', '{self.category}',
+                    '{self.kondisi}', {self.qty}, {self.price})
+        """
+
+        with psycopg.connect(f"dbname=product_catalogue_test user=avcwisesa") as conn:
+
+            with conn.cursor() as cur:
+
+                cur.execute(sql.SQL(insert_query))
+
+                conn.commit()
