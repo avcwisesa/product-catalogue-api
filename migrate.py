17d63474dcf0116bbd5ac98014dd0b9431e79fad
Avicenna Wisesa <avcwisesa@gmail.com>
2023-12-16 23:31:51 +0800

    feat: add created_at and updated_at column

--- migrate.py
+++ migrate.py
@@ -2,6 +2,8 @@ import psycopg
 import sys
 import tomllib
 
+from model import Product
+
 from psycopg import sql
 
 env = sys.argv[1]
@@ -38,8 +40,35 @@ with psycopg.connect(f"dbname={DBNAME} user={USER}") as conn:
                 category VARCHAR(15) NOT NULL,
                 kondisi VARCHAR(15) NOT NULL,
                 qty INTEGER NOT NULL,
-                price INTEGER NOT NULL
-            )
+                price INTEGER NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+
+            CREATE FUNCTION trigger_set_timestamp()
+            RETURNS TRIGGER AS $$
+                BEGIN
+                    NEW.updated_at=now();
+                    RETURN NEW;
+                END;
+            $$
+            LANGUAGE plpgsql;
+
+            CREATE TRIGGER set_timestamp
+            BEFORE UPDATE ON product
+            FOR EACH ROW
+                EXECUTE PROCEDURE trigger_set_timestamp();
         """)
 
         conn.commit()
+
+seeds = [
+    Product("ABC-123", "Produk 1", "cat 1", "kond 1", 3, 2500),
+    Product("ABZ-456", "Produk 2", "cat 2", "kond 1", 4, 5000),
+    Product("ABX-543", "Produk 3", "cat 3", "kond 2", 7, 10000),
+    Product("ABX-544", "Produk 4", "cat 3", "kond 2", 7, 11000),
+    Product("ABX-545", "Produk 5", "cat 3", "kond 1", 5, 12000)
+]
+
+for seed in seeds:
+    seed.save()
