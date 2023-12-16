815a0b449019ebde875857c8239b3ba9aa037a33
Avicenna Wisesa <avcwisesa@gmail.com>
2023-12-16 23:11:54 +0800

    feat: add model functionality for product search

--- model.py
+++ model.py
@@ -22,20 +22,6 @@ class Product():
         self.qty = qty
         self.price = price
 
-    def save(self):
-        insert_query = f"""
-            INSERT INTO product (sku, title, category, kondisi, qty, price)
-            VALUES ('{self.sku}', '{self.title}', '{self.category}',
-                    '{self.kondisi}', {self.qty}, {self.price})
-        """
-
-        with Database.connection() as conn:
-            with conn.cursor() as cur:
-
-                cur.execute(sql.SQL(insert_query))
-
-                conn.commit()
-
     @classmethod
     def get_by_id(cls, id):
         get_query = f"""
@@ -54,3 +40,66 @@ class Product():
                     return None
                 else:
                     return Product(*result)
+
+    @classmethod
+    def search(cls, skus=None, titles=None, categories=None, conditions=None,
+               limit=10, offset=0):
+        products = []
+
+        search_query = cls._search_query(skus, titles, categories, conditions)
+        search_query += f" LIMIT {limit} OFFSET {offset}"
+
+        with Database.connection() as conn:
+
+            with conn.cursor() as cur:
+
+                cur.execute(sql.SQL(search_query))
+                results = cur.fetchall()
+
+                products = [Product(*result) for result in results]
+
+        return products
+
+    @classmethod
+    def _search_query(cls, skus=None, titles=None, categories=None, conditions=None):
+        search_query = f"""
+            SELECT sku, title, category, kondisi, qty, price, id
+            FROM product
+        """
+
+        search_parameters = []
+
+        if skus:
+            search_parameters.append(f"""sku IN (\'{ "','".join(skus) }\')
+                                     """)
+
+        if titles:
+            search_parameters.append(f"""title IN (\'{ "','".join(titles) }\')
+                                     """)
+
+        if categories:
+            search_parameters.append(f"""category IN (\'{ "','".join(categories) }\')
+                                     """)
+
+        if conditions:
+            search_parameters.append(f"""kondisi IN (\'{ "','".join(conditions) }\')
+                                     """)
+
+        if search_parameters:
+            search_query += f" WHERE { ' AND '.join(search_parameters) }"
+
+        return search_query
+
+    def save(self):
+        insert_query = f"""
+            INSERT INTO product (sku, title, category, kondisi, qty, price)
+            VALUES ('{self.sku}', '{self.title}', '{self.category}',
+                    '{self.kondisi}', {self.qty}, {self.price})
+        """
+
+        with Database.connection() as conn:
+            with conn.cursor() as cur:
+
+                cur.execute(sql.SQL(insert_query))
+
+                conn.commit()
